Fixed bug in distance computation and cleaned some things up
... but was uint8 loading which produced different values because of different
times of overflow.

_z.py
@@ -15,23 +15,21 @@ print 'Test data shape: ', X_test.shape, X_test.dtype
 print 'Test labels shape: ', y_test.shape, y_test.dtype
 
 # Subsample to save on time/space
-num_training = 500
-mask = range(num_training)
-X_train = X_train[mask]
-y_train = y_train[mask]
+num_training = 50000
+X_train = X_train[:num_training]
+y_train = y_train[:num_training]
 
 num_test = 100
-mask = range(num_test)
-X_test = X_test[mask]
-y_test = y_test[mask]
+X_test = X_test[:num_test]
+y_test = y_test[:num_test]
 
 # Reshape the image data into rows: each item in these arrays is a 3072-element
 # vector representing 3 colors per image pixel.
 X_train = np.reshape(X_train, (X_train.shape[0], -1))
 X_test = np.reshape(X_test, (X_test.shape[0], -1))
 
-print 'Training data shape: ', X_train.shape, X_train.dtype
-print 'Test data shape: ', X_test.shape, X_test.dtype
+print 'Reshaped training data shape: ', X_train.shape, X_train.dtype
+print 'Reshaped test data shape: ', X_test.shape, X_test.dtype
 
 import k_nearest_neighbor
 knn = k_nearest_neighbor.KNearestNeighbor()
@@ -39,22 +37,7 @@ knn.train(X_train, y_train)
 #dists = knn.compute_distances_two_loops(X_test)
 
 with timer.Timer('Computing distances...'):
-    dists = knn.compute_distances_two_loops(X_test)
-#print dists_one
-
-# To ensure that our vectorized implementation is correct, we make sure that it
-# agrees with the naive implementation. There are many ways to decide whether
-# two matrices are similar; one of the simplest is the Frobenius norm. In case
-# you haven't seen it before, the Frobenius norm of two matrices is the square
-# root of the squared sum of differences of all elements; in other words, reshape
-# the matrices into vectors and compute the Euclidean distance between them.
-dists_none = knn.compute_distances_no_loops(X_test)
-difference = np.linalg.norm(dists - dists_none, ord='fro')
-print 'Difference was: %f' % (difference, )
-if difference < 0.001:
-  print 'Good! The distance matrices are the same'
-else:
-  print 'Uh-oh! The distance matrices are different'
+    dists = knn.compute_distances_no_loops(X_test)
 
 with timer.Timer('Running label prediction...'):
     y_test_pred = knn.predict_labels(dists, k=5)

cifar10.py
@@ -5,8 +5,10 @@
 # See https://www.cs.toronto.edu/~kriz/cifar.html for information on the
 # data set and its format.
 #
-# * Xs (data) are arrays of 32x33x3 arrays of pixels (each a uint8 for color
-#   intensity)
+# * Xs (data) are arrays of 32x33x3 arrays of pixels. Logically, the values are
+#   between 0-255, so they would fit into a uint8. However, since we want to
+#   perform math on them without running into uint overflow and other problems,
+#   we load them as float64.
 # * ys (labels) are arrays of integers in the range 0-9
 import cPickle as pickle
 import numpy as np
@@ -19,7 +21,7 @@ def _load_CIFAR_batch(filename):
         datadict = pickle.load(f)
         X = datadict['data']
         Y = datadict['labels']
-        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("uint8")
+        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype('float64')
         Y = np.array(Y)
         return X, Y
 

k_nearest_neighbor.py
@@ -58,14 +58,38 @@ class KNearestNeighbor:
 
     def compute_distances_no_loops(self, X):
         """Fully-vectorized distance matrix computation."""
-        X_norm = np.sum(X ** 2, axis=1, keepdims=True)
-        print 'X_norm', X_norm.shape
+        # The sum in L2 distance is:
+        #
+        #   distance[i,j] = sqrt(Sum_p (X_train[i,p] - X[j,p])^2
+        #
+        # where 'p' is running over the pixels/colors vector.
+        #
+        # The expression inside the sum can be rewritten as:
+        #
+        #   X_train[i,p]^2 - 2*X_train[i,p]*X[j,p] + X[j,p]^2
+        #
+        # Note that the first and last items only depend on one of i or j, not
+        # both, so they can be broadcast over the result array. And the middle
+        # item can be computed as matrix multiplication between X_train and X
+        # (one of them transposed).
+        X_train_T = self.X_train.T
+
+        # First compute the "cross-correlation" item using matrix mul,
+        # transposing X_train since we want tests in rows and train in columns.
+        # The shape of this is (num_test,num_train), which is also the shape
+        # of the result.
+        cross = -2.0 * X.dot(X_train_T)
+
+        # Now compute the first item: norm of X_train. Sum all columns together,
+        # getting a row vector.
         X_train_norm = np.sum(self.X_train ** 2, axis=1)
-        print 'X_train_norm', X_train_norm.shape
-        cross = -2.0 * X.dot(self.X_train.T)
-        print 'cross', cross.shape
-        dists = np.sqrt(X_norm + cross + X_train_norm)
-        return dists
+
+        # Similarly for X, but this time the results go into a column vector so
+        # it gets broadcast per column of the result.
+        X_norm = np.sum(X ** 2, axis=1, keepdims=True)
+
+        # Finally sum up the parts and compute their sqrt.
+        return np.sqrt(X_norm + cross + X_train_norm)
 
     def predict_labels(self, dists, k=1):
         """Predict labels, given a distances matrix.

k_nearest_neighbor_test.py
@@ -1,15 +1,20 @@
 import numpy as np
+import sys, unittest
+from k_nearest_neighbor import KNearestNeighbor
 
-import k_nearest_neighbor
+class TestKNearestNeighborDistance(unittest.TestCase):
+    """Tests that distance computations all return the same result."""
+    def test_arange(self):
+        train = np.arange(150).reshape(5, -1)
+        test = np.square(np.arange(2, 122)).reshape(4, -1)
+        knn = KNearestNeighbor()
+        knn.train(train, None)
+        d_two = knn.compute_distances_two_loops(test)
+        d_one = knn.compute_distances_one_loop(test)
+        d_no = knn.compute_distances_no_loops(test)
+        self.assertAlmostEqual(0, np.linalg.norm(d_two - d_one, ord='fro'))
+        self.assertAlmostEqual(0, np.linalg.norm(d_no - d_one, ord='fro'))
+    
 
-knn = k_nearest_neighbor.KNearestNeighbor()
-
-train = np.arange(180).reshape(6, -1)
-test = np.arange(120).reshape(4, -1)
-
-knn.train(train, None)
-
-dtwo = knn.compute_distances_two_loops(test)
-dnone = knn.compute_distances_no_loops(test)
-
-print np.linalg.norm(dtwo - dnone, ord='fro')
+if __name__ == '__main__':
+    unittest.main()