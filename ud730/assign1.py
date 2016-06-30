afabdeefa05bd33236090c51ac56949a9507fc9f
Eli Bendersky <eliben@gmail.com>
2016-06-30 06:57:09 -0700

    Pickle results of shuffling for easier loading

--- ud730/assign1.py
+++ ud730/assign1.py
@@ -222,5 +222,23 @@ train_dataset, train_labels = randomize(train_dataset, train_labels)
 test_dataset, test_labels = randomize(test_dataset, test_labels)
 valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
 
-print(train_labels[133])
-show_image(train_dataset[133])
+# Save shuffled training/validation/test sets for reuse.
+pickle_file = 'notMNIST.pickle'
+
+try:
+  f = open(pickle_file, 'wb')
+  save = {
+    'train_dataset': train_dataset,
+    'train_labels': train_labels,
+    'valid_dataset': valid_dataset,
+    'valid_labels': valid_labels,
+    'test_dataset': test_dataset,
+    'test_labels': test_labels,
+    }
+  pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+  f.close()
+except Exception as e:
+  print('Unable to save data to', pickle_file, ':', e)
+  raise
+statinfo = os.stat(pickle_file)
+print('Compressed pickle size:', statinfo.st_size)
