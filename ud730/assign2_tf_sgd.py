813b937e2f676c1e2aa4959c6910d2a279a0a598
Eli Bendersky <eliben@gmail.com>
2016-07-06 06:16:53 -0700

    Added solution to the problem (hidden layer with ReLU)

--- ud730/assign2_tf_sgd.py
+++ ud730/assign2_tf_sgd.py
@@ -175,5 +175,80 @@ def run_sgd():
       print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
 
+def run_sgd_with_hidden_layer(num_steps=3001):
+    # The "problem" part of the assignment
+    batch_size = 128
+    nfeatures = image_size * image_size
+    nhidden = 1024
+
+    graph = tf.Graph()
+    with graph.as_default():
+      # Input data. For the training data, we use a placeholder that will be fed
+      # at run time with a training minibatch.
+      tf_train_dataset = tf.placeholder(tf.float32,
+                                        shape=(batch_size, nfeatures))
+      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_valid_dataset = tf.constant(valid_dataset)
+      tf_test_dataset = tf.constant(test_dataset)
+
+      # Variables.
+      # Input shape is batch_size x nfeatures
+      # w1 is hidden layer: nfeatures x nhidden
+      # b1 is: nhidden x 1
+      # w2 is output (softmax) layer: nhidden x num_labels
+      # b2 is: num_labels x 1
+      w1 = tf.Variable(tf.truncated_normal([nfeatures, nhidden]))
+      b1 = tf.Variable(tf.zeros([nhidden]))
+      w2 = tf.Variable(tf.truncated_normal([nhidden, num_labels]))
+      b2 = tf.Variable(tf.zeros([num_labels]))
+
+      # Training computation.
+      hidden_out = tf.nn.relu(tf.matmul(tf_train_dataset, w1) + b1)
+      logits = tf.matmul(hidden_out, w2) + b2
+      loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+
+      # Optimizer.
+      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+      # Predictions for the training, validation, and test data.
+      train_prediction = tf.nn.softmax(logits)
+      valid_prediction = tf.nn.softmax(
+              tf.matmul(
+                  tf.nn.relu(tf.matmul(tf_valid_dataset, w1) + b1),
+                  w2) + b2)
+      test_prediction = tf.nn.softmax(
+              tf.matmul(
+                  tf.nn.relu(tf.matmul(tf_test_dataset, w1) + b1),
+                  w2) + b2)
+
+    with tf.Session(graph=graph) as session:
+      writer = tf.train.SummaryWriter("/tmp/tflogs", session.graph_def)
+      tf.initialize_all_variables().run()
+      print("Initialized")
+      for step in range(num_steps):
+        # Pick an offset within the training data, which has been randomized.
+        # Note: we could use better randomization across epochs.
+        offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+        # Generate a minibatch.
+        batch_data = train_dataset[offset:(offset + batch_size), :]
+        batch_labels = train_labels[offset:(offset + batch_size), :]
+        # Prepare a dictionary telling the session where to feed the minibatch.
+        # The key of the dictionary is the placeholder node of the graph to be fed,
+        # and the value is the numpy array to feed to it.
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        _, l, predictions = session.run(
+          [optimizer, loss, train_prediction], feed_dict=feed_dict)
+        if (step % 500 == 0):
+          print("Minibatch loss at step %d: %f" % (step, l))
+          print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+          print("Validation accuracy: %.1f%%" % accuracy(
+            valid_prediction.eval(), valid_labels))
+      print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
+
+
 if __name__ == '__main__':
-    run_sgd()
+    #run_sgd()
+
+    # This emits logs for tensorboard.
+    run_sgd_with_hidden_layer(701)
