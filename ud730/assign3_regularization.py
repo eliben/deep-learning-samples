aafe4814ce03bb339ef96fe87c69c2d81f27e5db
Eli Bendersky <eliben@gmail.com>
2016-07-08 06:46:41 -0700

    Add more stuff / dropout params / exponential learning decay
    91.9% accuracy on test set

--- ud730/assign3_regularization.py
+++ ud730/assign3_regularization.py
@@ -109,7 +109,28 @@ def run_sgd_with_reg():
       print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
 
-def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
+def run_sgd_with_hidden_layer_with_reg(num_steps=3001,
+                                       learning_rate=0.1,
+                                       l2_reg_beta=0.0,
+                                       dropout_keep_prob=0.5):
+    """Run SGD with a hidden layer and regularization.
+
+    num_steps:
+        Number of steps to run SGD minibatch training. In each step a new
+        minibatch is fed through the network.
+
+    learning_rate:
+        SGD learning rate, passed direction to GradientDescentOptimizer.
+
+    l2_reg_beta:
+        The loss is regularized with L2 loss on all weights. This is the
+        regularization coefficient. Setting it to 0 means no L2 loss
+        regularization. Otherwise, small values work well.
+
+    dropout_keep_prob:
+        Dropout "keep" probability for outputs of the hidden layer. Set to 1 to
+        avoid dropout altogether.
+    """
     # The "problem" part of the assignment
     batch_size = 128
     nfeatures = image_size * image_size
@@ -121,11 +142,14 @@ def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
       # at run time with a training minibatch.
       tf_train_dataset = tf.placeholder(tf.float32,
                                         shape=(batch_size, nfeatures))
-      tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+      tf_train_labels = tf.placeholder(tf.float32,
+                                       shape=(batch_size, num_labels))
+      tf_dropout_keep_prob = tf.placeholder(tf.float32)
       tf_valid_dataset = tf.constant(valid_dataset)
       tf_test_dataset = tf.constant(test_dataset)
 
-      beta = tf.constant(0.005)
+      # setting beta=0 means no L2-loss regularization
+      beta = tf.constant(l2_reg_beta)
 
       # Variables.
       # Input shape is batch_size x nfeatures
@@ -138,15 +162,24 @@ def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
       w2 = tf.Variable(tf.truncated_normal([nhidden, num_labels]))
       b2 = tf.Variable(tf.zeros([num_labels]))
 
-      # Training computation.
-      hidden_out = tf.nn.relu(tf.matmul(tf_train_dataset, w1) + b1)
+      # Training computation. Adding dropout to hidden layer output.
+      hidden_out = tf.nn.dropout(
+        tf.nn.relu(tf.matmul(tf_train_dataset, w1) + b1),
+        keep_prob=tf_dropout_keep_prob)
       logits = tf.matmul(hidden_out, w2) + b2
       loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
       reg_loss = loss + beta * tf.nn.l2_loss(w1) + beta * tf.nn.l2_loss(w2)
 
       # Optimizer.
-      optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(reg_loss)
+      global_step = tf.Variable(0)  # count the number of steps taken.
+      learning_rate = tf.train.exponential_decay(
+        learning_rate=learning_rate,
+        global_step=global_step,
+        decay_steps=1000,
+        decay_rate=0.96) 
+      optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate).minimize(reg_loss, global_step=global_step)
 
       # Predictions for the training, validation, and test data.
       train_prediction = tf.nn.softmax(logits)
@@ -160,7 +193,10 @@ def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
                   w2) + b2)
 
     with tf.Session(graph=graph) as session:
-      writer = tf.train.SummaryWriter("/tmp/tflogs", session.graph_def)
+
+      # Enable this to emit logs for TensorBoard.
+      #writer = tf.train.SummaryWriter("/tmp/tflogs", session.graph_def)
+
       tf.initialize_all_variables().run()
       print("Initialized")
       for step in range(num_steps):
@@ -173,10 +209,13 @@ def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
         # Prepare a dictionary telling the session where to feed the minibatch.
         # The key of the dictionary is the placeholder node of the graph to be fed,
         # and the value is the numpy array to feed to it.
-        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels}
+        feed_dict = {
+            tf_train_dataset: batch_data,
+            tf_train_labels: batch_labels,
+            tf_dropout_keep_prob: dropout_keep_prob}
         _, l, predictions = session.run(
           [optimizer, reg_loss, train_prediction], feed_dict=feed_dict)
-        if (step % 500 == 0):
+        if (step % 100 == 0):
           print("Minibatch loss at step %d: %f" % (step, l))
           print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
           print("Validation accuracy: %.1f%%" % accuracy(
@@ -187,4 +226,8 @@ def run_sgd_with_hidden_layer_with_reg(num_steps=3001):
 if __name__ == '__main__':
     #run_sgd_with_reg()
 
-    run_sgd_with_hidden_layer_with_reg(1701)
+    run_sgd_with_hidden_layer_with_reg(
+        num_steps=3711,
+        learning_rate=0.01,
+        l2_reg_beta=0.005,
+        dropout_keep_prob=0.8)
