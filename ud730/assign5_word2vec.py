e12b47dd079739fc2ec464edcb2ae5029dde96bb
Eli Bendersky <eliben@gmail.com>
2016-07-14 06:17:15 -0700

    add more stuff from assignment 5

--- ud730/assign5_word2vec.py
+++ ud730/assign5_word2vec.py
@@ -13,17 +13,114 @@ from sklearn.manifold import TSNE
 
 url = 'http://mattmahoney.net/dc/'
 
+
 def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified %s' % filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-      'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified %s' % filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception('Failed to verify ' + filename +
+                        '. Can you get to it with a browser?')
+    return filename
+
 
 filename = maybe_download('text8.zip', 31344016)
+
+
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words"""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+
+words = read_data(filename)
+print('Data size %d, type=%s' % (len(words), type(words)))
+
+# Only the vocabulary_size most common words are retained in the dictionary. All
+# others are mapped to UNK.
+vocabulary_size = 50000
+
+
+def build_dataset(words):
+    """Returns:
+
+    data:
+        list of the same length as words, with each word replaced by a unique
+        numeric ID.
+    count:
+        counters for the vocabulary_size most common words in 'words'.
+    dictionary:
+        maps word->ID
+    reverse_dictionary:
+        maps ID->word. Note that if the Kth word in 'words' is WORD, and the
+        Kth ID in 'data' is 42, then reverse_dictionary[42] is WORD.
+    """
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count = unk_count + 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
+
+
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+
+print('Most common words (+UNK)', count[:5])
+print('Sample data', data[:10])
+del words  # Hint to reduce memory.
+
+data_index = 0
+
+
+def generate_batch(batch_size, num_skips, skip_window):
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+
+print('data:', [reverse_dictionary[di] for di in data[:8]])
+
+for num_skips, skip_window in [(2, 1), (4, 2)]:
+    data_index = 0
+    batch, labels = generate_batch(batch_size=8,
+                                   num_skips=num_skips,
+                                   skip_window=skip_window)
+    print('\nwith num_skips = %d and skip_window = %d:' %
+          (num_skips, skip_window))
+    print('    batch:', [reverse_dictionary[bi] for bi in batch])
+    print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
