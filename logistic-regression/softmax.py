5a8552793e3ac3e3a41bc18bcfccbd6210762eba
Eli Bendersky <eliben@gmail.com>
2016-09-16 06:35:06 -0700

    Add softmax & stable softmax code

---
+++ logistic-regression/softmax.py
@@ -0,0 +1,23 @@
+# Experiments with softmax and its numerical stability.
+
+import numpy as np
+
+
+def softmax(x):
+    """Compute the softmax of vector x."""
+    exps = np.exp(x)
+    return exps / np.sum(exps)
+
+
+def stablesoftmax(x):
+    """Compute the softmax of vector x in a numerically stable way."""
+    x -= np.max(x)
+    exps = np.exp(x)
+    return exps / np.sum(exps)
+
+
+if __name__ == '__main__':
+    #pa = [2945.0, 2945.5]
+    pa = [1000, 2000, 3000]
+    print(softmax(pa))
+    print(stablesoftmax(pa))
