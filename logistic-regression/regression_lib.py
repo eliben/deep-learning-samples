Start putting in the correct code for softmax this time...
We need a matrix X so all dimensions are changed

logistic-regression/regression_lib.py
@@ -75,67 +75,29 @@ def sigmoid(z):
                         np.exp(z) / (1 + np.exp(z)))
 
 
-def softmax(z):
-    """Computes softmax function.
-
-    z: array of input values.
-
-    Returns an array of outputs with the same shape as z."""
-    # For numerical stability: make the maximum of z's to be 0.
-    shiftz = z - np.max(z)
-    exps = np.exp(shiftz)
-    return exps / np.sum(exps)
-
-
-def softmax_gradient(z):
-    """Computes the gradient of the softmax function.
-
-    z: array of input values where the gradient is computed.
-
-    Returns the full Jacobian of S(z): D (N, N) where DjSi is the partial
-    derivative of Si w.r.t. input j.
+def softmax_cross_entropy_loss(X, y, W, reg_beta=0.0):
     """
-    Sz = softmax(z)
-    # -SjSi can be computed using an outer product between Sz and itself. Then
-    # we add back Si for the i=j cases by adding a diagonal matrix with the
-    # values of Si on its diagonal.
-    dz = -np.outer(Sz, Sz) + np.diag(Sz.flatten())
-    return dz
-
-
-def softmax_layer(x, W):
-    """Computes a "softmax layer" for input vector x and weight matrix W.
-
-    A softmax layer is a fully connected layer followed by the softmax function.
-    Mathematically it's softmax(W.dot(x)).
-
-    x: (N, 1) input vector with N features.
-    W: (T, N) matrix of weights for N features and T output classes.
-
-    Returns s (T, 1) the result of applying softmax to W.dot(x)
+    X: (k, n) k rows of data items, each having n features; augmented.
+    y: (k,) a number in the closed interval [0..t-1] - the correct class, for
+            each of the k inputs.
+    W: (n, t) t is the number of classes.
     """
-    logits = W.dot(x)
-    return softmax(logits)
-
-
-def softmax_cross_entropy_loss_gradient(x, W, y):
-    """Computes the gradient of a cross-entropy loss for a softmax layer.
-
-    x: (N, 1) input
-    W: (T, N) weights
-    y: (T, 1) correct labels (one-hot vector with one element 1.0, others 0.0)
-
-    Returns D (1, N * T)
-    """
-    N = x.shape[0]
-    T = W.shape[0]
-    S = softmax_layer(x, W)
-    D = np.zeros(N * T)
-    yindex = np.argwhere(y == 1)[0, 0]
-    for i in range(T):
-        for j in range(N):
-            D[i*N + j] = (S[i, 0] - np.float32(i == yindex)) * x[j, 0]
-    return D
+    k = X.shape[0]
+    # Compute logits: (k, t)
+    logits = X.dot(W)
+    print(logits.shape)
+    print(logits)
+    # Vectorized sofmax computation on every row of logits. The normalization
+    # is done as a sum of columns, which is then broadcast over exp_logits.
+    # The result is also (k, t)
+    exp_logits = np.exp(logits)
+    softmax = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+    print(softmax.shape)
+    print(softmax)
+    logprobs = -np.log(softmax[range(k), y])
+    print(logprobs)
+    loss = np.sum(logprobs) / k
+    return loss
 
 
 def predict_logistic_probability(X, theta):

logistic-regression/regression_lib_test.py
@@ -300,48 +300,6 @@ class TestCrossEntropyBinaryLoss(unittest.TestCase):
         np.testing.assert_allclose(grad, gradnum, rtol=1e-4)
 
 
-class TestSoftmaxGradient(unittest.TestCase):
-    def checkSoftmaxGradientSimpleVsVec(self, z):
-        dz_vec = softmax_gradient(z)
-        dz_simple = softmax_gradient_simple(z)
-        np.testing.assert_allclose(dz_vec, dz_simple)
-
-    def test_simple_vs_numerical(self):
-        z = np.array([
-            [0.2],
-            [0.9],
-            [-0.3],
-            [-0.5]])
-        grad = softmax_gradient_simple(z)
-
-        for i in range(z.shape[0]):
-            # Compute numerical gradient for output Si w.r.t. all inputs
-            # j=0...N-1; this computes one row of the jacobian.
-            gradnum = eval_numerical_gradient(lambda z: softmax(z)[i, 0], z)
-            np.testing.assert_allclose(grad[i, :].flatten(),
-                                       gradnum.flatten(),
-                                       rtol=1e-4)
-
-    def test_simple_vs_vectorized_small(self):
-        z = np.array([
-            [0.2],
-            [0.9]])
-        self.checkSoftmaxGradientSimpleVsVec(z)
-
-    def test_simple_vs_vectorized_larger(self):
-        z = np.array([
-            [1.2],
-            [0],
-            [-0.01],
-            [2.12],
-            [-0.9]])
-        self.checkSoftmaxGradientSimpleVsVec(z)
-
-    def test_simple_vs_vectorized_random(self):
-        z = np.random.uniform(low=-2.0, high=2.0, size=(100,1))
-        self.checkSoftmaxGradientSimpleVsVec(z)
-
-
 class TestPredictBinary(unittest.TestCase):
     def test_simple(self):
         # Make sure positive gets +1, negative -1 and zero also gets +1.
@@ -469,6 +427,23 @@ class TestFeatureNormalize(unittest.TestCase):
         np.testing.assert_equal(mu, np.array([4, 6]))
         np.testing.assert_equal(sigma, np.array([1, 1]))
         np.testing.assert_equal(X_norm, np.array([[-1, 0], [1, 0]]))
+
+
+class TestSoftmaxCrossEntropyLoss(unittest.TestCase):
+    def test_trivial(self):
+        # Compares loss with hard-coded results from the CS231n sample
+        # at http://cs231n.github.io/linear-classify/
+        X = np.array([
+            [1.0, -15, 22, -44, 56]])
+        W = np.array([
+            [0.0, 0.2, -0.3],
+            [0.01, 0.7, 0.0],
+            [-0.05, 0.2, -0.45],
+            [0.1, 0.05, -0.2],
+            [0.05, 0.16, 0.03]])
+        y = np.array([2])
+        loss = softmax_cross_entropy_loss(X, y, W)
+        np.testing.assert_allclose(loss, 1.04, rtol=1e-3)
 
 
 if __name__ == '__main__':