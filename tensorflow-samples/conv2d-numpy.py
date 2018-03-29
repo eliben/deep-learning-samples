58ab78346b6e0393b6c19ca1ef5fe8cbfbb9162e
Eli Bendersky <eliben@gmail.com>
2018-03-29 07:02:37 -0700

    Adding comments

--- tensorflow-samples/conv2d-numpy.py
+++ tensorflow-samples/conv2d-numpy.py
@@ -9,14 +9,33 @@ import tensorflow as tf
 
 
 def conv2d_single_channel(input, w):
+    """Two-dimensional convolution of a single channel.
+
+    Uses SAME padding with 0s, a stride of 1 and no dilation.
+
+    input: input array with shape (height, width)
+    w: filter array with shape (fd, fd) with odd fd.
+
+    Returns a result with the same shape as input.
+    """
+    assert w.shape[0] == w.shape[1] and w.shape[0] % 2 == 1
+
+    # SAME padding with zeros: creating a new padded array to simplify index
+    # calculations and to avoid checking boundary conditions in the inner loop.
+    # padded_input is like input, but padded on all sides with
+    # half-the-filter-width of zeros.
     boundary_width = w.shape[0] // 2
     padded_input = np.pad(input,
                           pad_width=boundary_width,
                           mode='constant',
                           constant_values=0)
+
     output = np.zeros_like(input)
     for i in range(output.shape[0]):
         for j in range(output.shape[1]):
+            # This inner double loop computes every output element, by
+            # multiplying the corresponding window into the input with the
+            # filter.
             for fi in range(w.shape[0]):
                 for fj in range(w.shape[1]):
                     output[i, j] += padded_input[i + fi, j + fj] * w[fi, fj]
