fd24909f6d26ab120025d9b776c2c436eb1d7dc8
Eli Bendersky <eliben@gmail.com>
2018-04-12 06:03:30 -0700

    LSTM cell working

--- coursera-sequence-models/week-1-building-recurrent-network/rnn.py
+++ coursera-sequence-models/week-1-building-recurrent-network/rnn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from rnn_utils import softmax
+from rnn_utils import sigmoid, softmax
 
 
 def rnn_cell_forward(xt, a_prev, parameters):
@@ -96,37 +96,98 @@ def rnn_forward(x, a0, parameters):
     return a, y_pred, caches
 
 
+def lstm_cell_forward(xt, a_prev, c_prev, parameters):
+    """
+    Implement a single forward step of the LSTM-cell as described in Figure (4)
+
+    Arguments:
+    xt -- your input data at timestep "t", numpy array of shape (n_x, m).
+    a_prev -- Hidden state at timestep "t-1", numpy array of shape (n_a, m)
+    c_prev -- Memory state at timestep "t-1", numpy array of shape (n_a, m)
+    parameters -- python dictionary containing:
+      Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+      bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+      Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+      bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+      Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+      bc --  Bias of the first "tanh", numpy array of shape (n_a, 1)
+      Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+      bo --  Bias of the output gate, numpy array of shape (n_a, 1)
+      Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+      by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a_next -- next hidden state, of shape (n_a, m)
+    c_next -- next memory state, of shape (n_a, m)
+    yt_pred -- prediction at timestep "t", numpy array of shape (n_y, m)
+    cache -- tuple of values needed for the backward pass,
+             contains (a_next, c_next, a_prev, c_prev, xt, parameters)
+
+    Note: ft/it/ot stand for the forget/update/output gates, cct stands for the
+          candidate value (c tilde), c stands for the memory value
+    """
+
+    # Retrieve parameters from "parameters"
+    Wf = parameters["Wf"]
+    bf = parameters["bf"]
+    Wi = parameters["Wi"]
+    bi = parameters["bi"]
+    Wc = parameters["Wc"]
+    bc = parameters["bc"]
+    Wo = parameters["Wo"]
+    bo = parameters["bo"]
+    Wy = parameters["Wy"]
+    by = parameters["by"]
+
+    # Retrieve dimensions from shapes of xt and Wy
+    n_x, m = xt.shape
+    n_y, n_a = Wy.shape
+
+    # Concatenate a_prev and xt
+    concat = np.zeros((n_a + n_x, m))
+    concat[: n_a, :] = a_prev
+    concat[n_a :, :] = xt
+
+    # Compute values for ft, it, cct, c_next, ot, a_next using the formulas
+    # given figure (4)
+    ft = sigmoid(np.dot(Wf, concat) + bf)
+    it = sigmoid(np.dot(Wi, concat) + bi)
+    cct = np.tanh(np.dot(Wc, concat) + bc)
+    c_next = ft * c_prev + it * cct
+    ot = sigmoid(np.dot(Wo, concat) + bo)
+    a_next = ot * np.tanh(c_next)
+
+    yt_pred = softmax(np.dot(Wy, a_next) + by)
+
+    # store values needed for backward propagation in cache
+    cache = (a_next, c_next, a_prev, c_prev, ft, it, cct, ot, xt, parameters)
+    return a_next, c_next, yt_pred, cache
+
+
 if __name__ == '__main__':
-    #np.random.seed(1)
-    #xt = np.random.randn(3,10)
-    #a_prev = np.random.randn(5,10)
-    #Waa = np.random.randn(5,5)
-    #Wax = np.random.randn(5,3)
-    #Wya = np.random.randn(2,5)
-    #ba = np.random.randn(5,1)
-    #by = np.random.randn(2,1)
-    #parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
-
-    #a_next, yt_pred, cache = rnn_cell_forward(xt, a_prev, parameters)
-    #print("a_next[4] = ", a_next[4])
-    #print("a_next.shape = ", a_next.shape)
-    #print("yt_pred[1] =", yt_pred[1])
-    #print("yt_pred.shape = ", yt_pred.shape)
-
     np.random.seed(1)
-    x = np.random.randn(3,10,4)
-    a0 = np.random.randn(5,10)
-    Waa = np.random.randn(5,5)
-    Wax = np.random.randn(5,3)
-    Wya = np.random.randn(2,5)
-    ba = np.random.randn(5,1)
+    xt = np.random.randn(3,10)
+    a_prev = np.random.randn(5,10)
+    c_prev = np.random.randn(5,10)
+    Wf = np.random.randn(5, 5+3)
+    bf = np.random.randn(5,1)
+    Wi = np.random.randn(5, 5+3)
+    bi = np.random.randn(5,1)
+    Wo = np.random.randn(5, 5+3)
+    bo = np.random.randn(5,1)
+    Wc = np.random.randn(5, 5+3)
+    bc = np.random.randn(5,1)
+    Wy = np.random.randn(2,5)
     by = np.random.randn(2,1)
-    parameters = {"Waa": Waa, "Wax": Wax, "Wya": Wya, "ba": ba, "by": by}
 
-    a, y_pred, caches = rnn_forward(x, a0, parameters)
-    print("a[4][1] = ", a[4][1])
-    print("a.shape = ", a.shape)
-    print("y_pred[1][3] =", y_pred[1][3])
-    print("y_pred.shape = ", y_pred.shape)
-    print("caches[1][1][3] =", caches[1][1][3])
-    print("len(caches) = ", len(caches))
+    parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+    a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+    print("a_next[4] = ", a_next[4])
+    print("a_next.shape = ", c_next.shape)
+    print("c_next[2] = ", c_next[2])
+    print("c_next.shape = ", c_next.shape)
+    print("yt[1] =", yt[1])
+    print("yt.shape = ", yt.shape)
+    print("cache[1][3] =", cache[1][3])
+    print("len(cache) = ", len(cache))
