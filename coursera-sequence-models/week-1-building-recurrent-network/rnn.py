ffac6da8eb3a8fa499866a9b3fe5574e01cb50d7
Eli Bendersky <eliben@gmail.com>
2018-04-12 06:11:40 -0700

    lstm_forward working

--- coursera-sequence-models/week-1-building-recurrent-network/rnn.py
+++ coursera-sequence-models/week-1-building-recurrent-network/rnn.py
@@ -164,11 +164,68 @@ def lstm_cell_forward(xt, a_prev, c_prev, parameters):
     return a_next, c_next, yt_pred, cache
 
 
+def lstm_forward(x, a0, parameters):
+    """
+    Implement the forward propagation of the recurrent neural network using an
+    LSTM-cell described in Figure (3).
+
+    Arguments:
+    x -- Input data for every time-step, of shape (n_x, m, T_x).
+    a0 -- Initial hidden state, of shape (n_a, m)
+    parameters -- python dictionary containing:
+      Wf -- Weight matrix of the forget gate, numpy array of shape (n_a, n_a + n_x)
+      bf -- Bias of the forget gate, numpy array of shape (n_a, 1)
+      Wi -- Weight matrix of the update gate, numpy array of shape (n_a, n_a + n_x)
+      bi -- Bias of the update gate, numpy array of shape (n_a, 1)
+      Wc -- Weight matrix of the first "tanh", numpy array of shape (n_a, n_a + n_x)
+      bc -- Bias of the first "tanh", numpy array of shape (n_a, 1)
+      Wo -- Weight matrix of the output gate, numpy array of shape (n_a, n_a + n_x)
+      bo -- Bias of the output gate, numpy array of shape (n_a, 1)
+      Wy -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+      by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+
+    Returns:
+    a -- Hidden states for every time-step, numpy array of shape (n_a, m, T_x)
+    y -- Predictions for every time-step, numpy array of shape (n_y, m, T_x)
+    caches -- tuple of values needed for the backward pass, contains (list of all the caches, x)
+    """
+
+    # Initialize "caches", which will track the list of all the caches
+    caches = []
+
+    # Retrieve dimensions from shapes of x and parameters['Wy'] (≈2 lines)
+    n_x, m, T_x = x.shape
+    n_y, n_a = Wy.shape
+
+    a = np.zeros((n_a, m, T_x))
+    c = np.zeros((n_a, m, T_x))
+    y = np.zeros((n_y, m, T_x))
+
+    # Initialize a_next and c_next
+    a_next = a0
+    c_next = np.zeros_like(a_next)
+
+    # loop over all time-steps
+    for t in range(T_x):
+        # Update next hidden state, next memory state, compute the prediction,
+        # get the cache.
+        a_next, c_next, yt, cache = lstm_cell_forward(x[:,:,t], a_next, c_next,
+                                                      parameters)
+        a[:,:,t] = a_next
+        y[:,:,t] = yt
+        c[:,:,t] = c_next
+        caches.append(cache)
+
+    # store values needed for backward propagation in cache
+    caches = (caches, x)
+
+    return a, y, c, caches
+
+
 if __name__ == '__main__':
     np.random.seed(1)
-    xt = np.random.randn(3,10)
-    a_prev = np.random.randn(5,10)
-    c_prev = np.random.randn(5,10)
+    x = np.random.randn(3,10,7)
+    a0 = np.random.randn(5,10)
     Wf = np.random.randn(5, 5+3)
     bf = np.random.randn(5,1)
     Wi = np.random.randn(5, 5+3)
@@ -182,12 +239,39 @@ if __name__ == '__main__':
 
     parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
 
-    a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
-    print("a_next[4] = ", a_next[4])
-    print("a_next.shape = ", c_next.shape)
-    print("c_next[2] = ", c_next[2])
-    print("c_next.shape = ", c_next.shape)
-    print("yt[1] =", yt[1])
-    print("yt.shape = ", yt.shape)
-    print("cache[1][3] =", cache[1][3])
-    print("len(cache) = ", len(cache))
+    a, y, c, caches = lstm_forward(x, a0, parameters)
+    print("a[4][3][6] = ", a[4][3][6])
+    print("a.shape = ", a.shape)
+    print("y[1][4][3] =", y[1][4][3])
+    print("y.shape = ", y.shape)
+    print("caches[1][1[1]] =", caches[1][1][1])
+    print("c[1][2][1]", c[1][2][1])
+    print("len(caches) = ", len(caches))
+
+
+    #np.random.seed(1)
+    #xt = np.random.randn(3,10)
+    #a_prev = np.random.randn(5,10)
+    #c_prev = np.random.randn(5,10)
+    #Wf = np.random.randn(5, 5+3)
+    #bf = np.random.randn(5,1)
+    #Wi = np.random.randn(5, 5+3)
+    #bi = np.random.randn(5,1)
+    #Wo = np.random.randn(5, 5+3)
+    #bo = np.random.randn(5,1)
+    #Wc = np.random.randn(5, 5+3)
+    #bc = np.random.randn(5,1)
+    #Wy = np.random.randn(2,5)
+    #by = np.random.randn(2,1)
+
+    #parameters = {"Wf": Wf, "Wi": Wi, "Wo": Wo, "Wc": Wc, "Wy": Wy, "bf": bf, "bi": bi, "bo": bo, "bc": bc, "by": by}
+
+    #a_next, c_next, yt, cache = lstm_cell_forward(xt, a_prev, c_prev, parameters)
+    #print("a_next[4] = ", a_next[4])
+    #print("a_next.shape = ", c_next.shape)
+    #print("c_next[2] = ", c_next[2])
+    #print("c_next.shape = ", c_next.shape)
+    #print("yt[1] =", yt[1])
+    #print("yt.shape = ", yt.shape)
+    #print("cache[1][3] =", cache[1][3])
+    #print("len(cache) = ", len(cache))
