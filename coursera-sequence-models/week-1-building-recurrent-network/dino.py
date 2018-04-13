dcf5cab342cc885ef4079b4f83d5da46cba48133
Eli Bendersky <eliben@gmail.com>
2018-04-13 06:32:47 -0700

    Added optimize

--- coursera-sequence-models/week-1-building-recurrent-network/dino.py
+++ coursera-sequence-models/week-1-building-recurrent-network/dino.py
@@ -31,7 +31,7 @@ def clip(gradients, maxValue):
                                  gradients['dby'])
    
     for gradient in [dWax, dWaa, dWya, db, dby]:
-        np.clip(gradient, a_min=-10, a_max=10, out=gradient)
+        np.clip(gradient, a_min=-maxValue, a_max=maxValue, out=gradient)
     
     gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
     return gradients
@@ -112,6 +112,50 @@ def sample(parameters, char_to_ix, seed):
         indices.append(char_to_ix['\n'])
     
     return indices
+
+
+def optimize(X, Y, a_prev, parameters, learning_rate = 0.01):
+    """
+    Execute one step of the optimization to train the model.
+    
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to
+         a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index
+         to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+      Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+      Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+      Wya -- Weight matrix relating the hidden-state to the output,
+             numpy array of shape (n_y, n_a)
+      b --  Bias, numpy array of shape (n_a, 1)
+      by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+    
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+      dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+      dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+      dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+      db -- Gradients of bias vector, of shape (n_a, 1)
+      dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+    # Forward propagate through time
+    loss, cache = rnn_forward(X, Y, a_prev, parameters, vocab_size)
+    
+    # Backpropagate through time
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+    
+    # Clip your gradients between -5 (min) and 5 (max)
+    gradients = clip(gradients, 5)
+    
+    # Update parameters
+    parameters = update_parameters(parameters, gradients, learning_rate)
+    
+    return loss, gradients, a[len(X)-1]
 
 
 if __name__ == '__main__':
