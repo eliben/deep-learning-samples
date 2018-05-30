6ecd9f127d249e14f3194d63efe97dc23b4ed3fd
Eli Bendersky <eliben@gmail.com>
2018-05-30 06:02:40 -0700

    Start writing the forward pass and replace *_size by capital letters

--- min-char-rnn/min-char-lstm.py
+++ min-char-rnn/min-char-lstm.py
@@ -16,7 +16,8 @@ with open(filename, 'r') as f:
 
 # All unique characters / entities in the data set.
 chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
+data_size = len(data)
+V = vocab_size = len(chars)
 print('data has %d characters, %d unique.' % (data_size, vocab_size))
 
 # Each character in the vocabulary gets a unique integer index assigned, in the
@@ -30,12 +31,12 @@ print('ix_to_char', ix_to_char)
 # Hyperparameters.
 
 # Size of hidden state vectors; applies to h and c.
-hidden_size = 100
+H = hidden_size = 100
 seq_length = 16 # number of steps to unroll the LSTM for
 learning_rate = 1e-1
 
-# Size of combined state (HV): input with hidden.
-combined_size = hidden_size + vocab_size
+# Size of combined state: input with hidden.
+HV = H + V
 
 # Stop when processed this much data
 MAX_DATA = 1000000
@@ -44,13 +45,41 @@ MAX_DATA = 1000000
 # initialized randomly; biases initialized to 0.
 # Inputs are characters one-hot encoded in a vocab-sized vector.
 # Dimensions: H = hidden_size, V = vocab_size, HV = hidden_size + vocab_size
-Wf = np.random.randn(hidden_size, combined_size) * 0.01
-bf = np.zeros((hidden_size, 1))
-Wi = np.random.randn(hidden_size, combined_size) * 0.01
-bi = np.zeros((hidden_size, 1))
-Wct = np.random.randn(hidden_size, combined_size) * 0.01
-bct = np.zeros((hidden_size, 1))
-Wo = np.random.randn(hidden_size, combined_size) * 0.01
-bo = np.zeros((hidden_size, 1))
-Why = np.random.randn(vocab_size, hidden_size) * 0.01
-by = np.zeros((vocab_size, 1))
+Wf = np.random.randn(H, HV) * 0.01
+bf = np.zeros((H, 1))
+Wi = np.random.randn(H, HV) * 0.01
+bi = np.zeros((H, 1))
+Wct = np.random.randn(H, HV) * 0.01
+bct = np.zeros((H, 1))
+Wo = np.random.randn(H, HV) * 0.01
+bo = np.zeros((H, 1))
+Why = np.random.randn(V, H) * 0.01
+by = np.zeros((V, 1))
+
+def lossFun(inputs, targets, hprev, cprev):
+    """Runs forward and backward passes through the RNN.
+ 
+      TODO: keep me updated!
+      inputs, targets: Lists of integers. For some i, inputs[i] is the input
+                       character (encoded as an index into the ix_to_char map)
+                       and targets[i] is the corresponding next character in the
+                       training data (similarly encoded).
+      hprev: Hx1 array of initial hidden state
+      cprev: Hx1 array of initial hidden state
+ 
+      returns: ? loss, gradients on model parameters, and last hidden state
+    """
+    loss = 0
+    # Forward pass
+    for t in range(len(inputs)):
+        # Input at time step t is xs[t]. Prepare a one-hot encoded vector of
+        # shape (V, 1). inputs[t] is the index where the 1 goes.
+        xs[t] = np.zeros((V, 1))
+        xs[t][inputs[t]] = 1
+
+        # hprev and xs[t] are column vector; stack them together into a "taller"
+        # column vector.
+        xh = np.row_stack(hprev, xs[t])
+
+
+
