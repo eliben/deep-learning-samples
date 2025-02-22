4cd64a02c3438f36883a3b2aaf811456fa6910cf
Eli Bendersky <eliben@gmail.com>
2025-02-22 09:22:46 -0800

    batched works

--- transformer-attention/test_selfattention.py
+++ transformer-attention/test_selfattention.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-from selfattention import self_attention, self_attention_cols
+from selfattention import *
 
 
 def test_shapes_cols():
@@ -51,6 +51,46 @@ def test_rows_values():
             [19.1712, 19.8908, 20.6105, 21.3302],
             [19.1783, 19.8982, 20.6182, 21.3381],
             [19.1797, 19.8997, 20.6196, 21.3396],
+        ]
+    )
+    assert np.allclose(y, want, atol=1e-3)
+
+
+def test_batched():
+    B = 3
+    N = 4
+    D = 2
+
+    # This is for testing shape
+    x = np.random.randn(B, N, D)
+    Wk = np.random.randn(D, D)
+    Wq = np.random.randn(D, D)
+    Wv = np.random.randn(D, D)
+    y = self_attention_batched(x, Wk, Wq, Wv)
+    assert y.shape == (B, N, D)
+
+    # Testing values
+    x = np.linspace(0.1, 5.4, B * N * D).reshape(B, N, D)
+    Wk = np.linspace(0.1, 0.8, D * D).reshape((D, D)).T
+    Wq = np.linspace(1.1, 1.8, D * D).reshape((D, D)).T
+    Wv = np.linspace(2.1, 2.8, D * D).reshape((D, D)).T
+    y = self_attention_batched(x, Wk, Wq, Wv)
+
+    want = np.array(
+        [
+            [[5.0515, 6.1093], [6.3565, 7.6890], [6.8308, 8.2633], [6.9991, 8.4669]],
+            [
+                [15.2371, 18.4392],
+                [15.2638, 18.4716],
+                [15.2750, 18.4852],
+                [15.2798, 18.4909],
+            ],
+            [
+                [23.4546, 28.3867],
+                [23.4554, 28.3878],
+                [23.4558, 28.3882],
+                [23.4560, 28.3884],
+            ],
         ]
     )
     assert np.allclose(y, want, atol=1e-3)
