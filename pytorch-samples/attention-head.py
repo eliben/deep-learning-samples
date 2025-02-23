f567b0c768f9675f91ee511030a56edf639fa780
Eli Bendersky <eliben@gmail.com>
2025-02-22 16:16:18 -0800

    add multihead

--- pytorch-samples/attention-head.py
+++ pytorch-samples/attention-head.py
@@ -2,6 +2,17 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+device = (
+    torch.accelerator.current_accelerator().type
+    if torch.accelerator.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
 
 # Attention head taken from https://github.com/karpathy/ng-video-lecture/blob/master/gpt.py
 # with masking/dropout removed
@@ -35,6 +46,23 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    """multiple heads of self-attention in parallel"""
+
+    def __init__(self, C, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(C, head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, C)
+        # self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        # out = self.dropout(self.proj(out))
+        return out
+
+
+print("---- Single attention head, B=1 ----")
 B = 1
 T = 6
 C = 4
@@ -58,6 +86,67 @@ out = head(x)
 
 print(out.shape)
 print(out)
-# x = torch.randn(2, 10)
-# out = model(x)
-# print(out)
+
+print("---- Single attention head, B=3 ----")
+B = 3
+T = 4
+C = 2
+HS = 2
+
+x = torch.linspace(0.1, 5.4, B * T * C).reshape(B, T, C)
+print(x)
+
+bhead = Head(C, HS)
+
+kseq = torch.linspace(0.1, 0.8, C * HS).view(bhead.key.weight.shape)
+qseq = torch.linspace(1.1, 1.8, C * HS).view(bhead.query.weight.shape)
+vseq = torch.linspace(2.1, 2.8, C * HS).view(bhead.value.weight.shape)
+
+with torch.no_grad():
+    bhead.key.weight.copy_(kseq)
+    bhead.query.weight.copy_(qseq)
+    bhead.value.weight.copy_(vseq)
+
+out = bhead(x)
+print(out.shape)
+print(out)
+
+print("---- Multi-head attention, B=3 ----")
+B = 3
+T = 4
+C = 6
+HS = 2
+NH = 3
+
+x = torch.linspace(0.1, 8.4, B * T * C).reshape(B, T, C)
+print(x)
+
+mhead = MultiHeadAttention(C, NH, HS)
+
+kseqs = [
+    torch.linspace(i + 0.1, i + 0.8, C * HS).view(mhead.heads[0].key.weight.shape)
+    for i in range(NH)
+]
+
+qseqs = [
+    torch.linspace(i + 3.1, i + 3.8, C * HS).view(mhead.heads[0].key.weight.shape)
+    for i in range(NH)
+]
+
+vseqs = [
+    torch.linspace(i + 6.1, i + 6.8, C * HS).view(mhead.heads[0].key.weight.shape)
+    for i in range(NH)
+]
+
+pseq = torch.linspace(9.1, 9.8, C * HS * NH).view(mhead.proj.weight.shape)
+
+with torch.no_grad():
+    for i in range(NH):
+        mhead.heads[i].key.weight.copy_(kseqs[i])
+        mhead.heads[i].query.weight.copy_(qseqs[i])
+        mhead.heads[i].value.weight.copy_(vseqs[i])
+    mhead.proj.weight.copy_(pseq)
+
+out = mhead(x)
+print(out.shape)
+print(out)
