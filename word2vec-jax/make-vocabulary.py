9b09ec4d8bd420580124127321c79b8f2b5fa133
Eli Bendersky <eliben@gmail.com>
2025-03-24 07:22:35 -0700

    Starting project

---
+++ word2vec-jax/make-vocabulary.py
@@ -0,0 +1,24 @@
+from collections import Counter
+import random
+import math
+
+def read_words_from_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read().split()
+
+
+def subsample(words, threshold=1e-4):
+    word_counts = Counter(words)
+    total_count = len(words)
+    freqs = {word: count / total_count for word, count in word_counts.items()}
+    p_keep = {word: math.sqrt(threshold / freqs[word]) 
+              if freqs[word] > threshold else 1
+              for word in word_counts}
+    return [word for word in words if random.random() < p_keep[word]]
+
+if __name__ == '__main__':
+    words = read_words_from_file('text8')
+    print('Number of words:', len(words))
+
+    ss = subsample(words)
+    print('Number of words after subsampling:', len(ss))
