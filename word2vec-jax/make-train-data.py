6288885c6eb6f9b0df40c8866c59874f2fcf980b
Eli Bendersky <eliben@gmail.com>
2025-03-24 20:53:17 -0700

    train vector generation

--- word2vec-jax/make-train-data.py
+++ word2vec-jax/make-train-data.py
@@ -1,3 +1,11 @@
+# Takes the raw dataset (text8) and creates training data from it, by
+# subsampling frequent words and creating a vocabulary. The training data is
+# saved to a pickle file as the dict
+# {
+#     "train_data": list of words,
+#     "vocab": dict from word to ID
+# }
+#
 from collections import Counter
 import random
 import math
@@ -6,7 +14,7 @@ import pickle
 
 def read_words_from_file(file_path):
     """Reads whitespace-separated words from a file.
-     
+
     Returns a list of words.
     """
     with open(file_path, "r") as file:
@@ -15,7 +23,7 @@ def read_words_from_file(file_path):
 
 def subsample(words, threshold=1e-4):
     """Subsample frequent words, return a new list of words.
-    
+
     Follows the subsampling procedure described in the paper "Distributed
     Representations of Words and Phrases and their Compositionality" by
     Mikolov et al. (2013).
@@ -35,7 +43,7 @@ def subsample(words, threshold=1e-4):
 
 def make_vocabulary(words, top_k=20000):
     """Creates a vocabulary from a list of words.
-    
+
     Keeps the top_k most common words and assigns an index to each word. The
     index 0 is reserved for the "<unk>" token.
     """
@@ -62,7 +70,7 @@ if __name__ == "__main__":
     train_data = [word for word in ss if word in vocab]
     print("Number of words in training data:", len(train_data))
 
-    output_filename = 'train-data.pickle'
+    output_filename = "train-data.pickle"
     print("Saving training data to", output_filename)
-    with open(output_filename, 'wb') as file:
-        pickle.dump({'train_data': train_data, 'vocab': vocab}, file)
+    with open(output_filename, "wb") as file:
+        pickle.dump({"train_data": train_data, "vocab": vocab}, file)
--- word2vec-jax/train.py
+++ word2vec-jax/train.py
@@ -1,4 +1,41 @@
+import numpy as np
 import pickle
+
+
+def generate_train_vectors(train_data, vocab, window_size=4, batch_size=128):
+    """Generates training vectors from a list of words and vocabulary.
+    
+    Generates (target_batch, context_batch) pairs, where target_batch is a
+    (batch_size,) array of target word IDs and context_batch is a (batch_size,)
+    array of corresponding contexts; each context is an (2*window_size,) array
+    of word IDs.
+
+    Stops when it runs out of data. The last batch may have fewer elements than
+    batch_size.
+    """
+    target_batch = []
+    context_batch = []
+
+    for i in range(len(train_data)):
+        if i + 2 * window_size >= len(train_data):
+            break
+
+        # 'i' is the index of the leftmost word in the context.
+        target_word = train_data[i + window_size]
+        left_context = train_data[i : i + window_size]
+        right_context = train_data[i + window_size + 1 : i + 2 * window_size + 1]
+        context_words = left_context + right_context
+        
+        if target_word not in vocab:
+            continue
+        target_batch.append(vocab.get(target_word, '<unk>'))
+        context_batch.append([vocab.get(word, '<unk>') for word in context_words])
+
+        if len(target_batch) == batch_size:
+            yield np.array(target_batch), np.array(context_batch)
+            target_batch = []
+            context_batch = []
+    
 
 if __name__ == "__main__":
     # Load train-data from pickle file
@@ -8,6 +45,17 @@ if __name__ == "__main__":
         vocab = data["vocab"]
     print("Number of words in train data:", len(train_data))
     print("Vocabulary size:", len(vocab))
-    print("First 10 words and their IDs:",
-          [(word, vocab[word]) for word in train_data[:10]])
-    
+    print(
+        "First 10 words and their IDs:",
+        [(word, vocab[word]) for word in train_data[:10]],
+    )
+
+    cnt = 0
+    for target_batch, context_batch in generate_train_vectors(train_data, vocab, batch_size=4):
+        cnt += 1
+        # print("Target batch:", target_batch)
+        # print("Context batch:", context_batch)
+        # print()
+
+    print("Number of batches:", cnt)
+
